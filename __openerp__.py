75da77aa3007b70aab09c62c4cfffda4a027cf4d
Yann L <yleroux@gmail.com>
2020-05-12 21:00:59 +0200

    0.0.1

--- __openerp__.py
+++ __openerp__.py
@@ -4,9 +4,11 @@
    'licence': 'LGPL-3',
    'version' : "1.0",
    'description' : 'Ajout de champs spécifiques pour les articles bières',
+   'summary':'Add some fields to products',
    'support':'yleroux@gmail.com',
    'depends' : ['sale'],
+   'data':['brewery_view.xml'],
    'category':'Products',
-   'website':'https://github.com/BreizhCat/',
+   'website':'https://github.com/BreizhCat/odoo_brewery',
    'installable' : True,
 }
--- brewery.py
+++ brewery.py
@@ -6,6 +6,10 @@ class Brewery_product_template(models.Model):
     _name = 'product.template'
     _inherit = 'product.template'
     
-    ibu_level = fields.Float(string='Amertume (IBU)')
-    abv_level = fields.Float(string='Teneur en alcool (ABV)')    
-    has_gluten = fields.Boolean(string='Contient du gluten' )
+    brewery_ibu_level   = fields.Float(string='Amertume (IBU)')
+    brewery_abv_level   = fields.Float(string='Teneur en alcool (ABV)')    
+    brewery_has_gluten  = fields.Boolean(string='Contient du gluten' )
+    brewery_is_bio      = fields.Boolean(string='Bio' )
+    brewery_is_beer     = fields.Boolean(string='Bière')
+    brewery_untappd_url = fields.Char("Url Untappd")
+    
